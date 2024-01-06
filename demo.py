55afd22646f620e6cfc4a4181c6352a90df293bf
Stefan Stefanov <stefanov.sm@abv.bg>
2024-01-06 13:31:24 +0200

    Explicit transaction control

--- demo.py
+++ demo.py
@@ -3,29 +3,26 @@ import psycopg
 import demo_settings as cfg
 import dbgw
 
-conn = psycopg.connect(cfg.CONNECTIONSTRING)
-db = dbgw.db_gateway(conn, cfg.SQLPATH+'methods.sql', cfg.SQLPATH+'more.methods.sql', autocommit = False)
+with psycopg.connect(cfg.CONNECTIONSTRING) as conn:
 
-res = db.named_recordset({'seq_from': 1000, 'seq_to': 1099, 'status': 5})
-print ('\r\nnamed_recordset:', json.dumps(res, indent = 2))
+  db = dbgw.db_gateway(conn, cfg.SQLPATH + 'methods.sql', cfg.SQLPATH + 'more.methods.sql', autocommit = False)
 
-res = db.positional_record(12345)
-print ('\r\npositional_record:', json.dumps(res, indent = 2))
+  res = db.named_recordset({'seq_from': 1000, 'seq_to': 1099, 'status': 5})
+  print ('\r\nnamed_recordset:', json.dumps(res, indent = 2))
 
-res = db.named_value({'seq':54321})
-print ('\r\nnamed_value:', res)
+  res = db.positional_record(12345)
+  print ('\r\npositional_record:', json.dumps(res, indent = 2))
 
-res = db.positional_none(200000, 11)
-print ('\r\npositional_none:', res)
+  res = db.named_value({'seq':54321})
+  print ('\r\nnamed_value:', res)
 
-db.commit()
+  res = db.positional_none(13, 123)
+  db.commit()
+  print ('\r\npositional_none:', res)
 
-res = db.Roman_64_numerals(10)
-print ('\r\nRoman_64_numerals:', json.dumps(res, indent = 2))
+  res = db.Roman_64_numerals(10)
+  print ('\r\nRoman_64_numerals:', json.dumps(res, indent = 2))
 
-res = db.cleanup({'seq':200000})
-print ('\r\ncleanup:', res)
-
-db.commit()
-
-conn.close()
+  res = db.cleanup({'seq':200000});
+  db.commit()
+  print ('\r\ncleanup:', res)
