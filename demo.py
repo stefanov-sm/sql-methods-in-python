c4df2332a35ac8da6a6050d42d4f6bca2433fd89
Stefan Stefanov <stefanov.sm@abv.bg>
2024-01-06 12:47:28 +0200

    Transaction control

--- demo.py
+++ demo.py
@@ -4,7 +4,7 @@ import demo_settings as cfg
 import dbgw
 
 conn = psycopg.connect(cfg.CONNECTIONSTRING)
-db = dbgw.db_gateway(conn, cfg.SQLPATH+'methods.sql', cfg.SQLPATH+'more.methods.sql')
+db = dbgw.db_gateway(conn, cfg.SQLPATH+'methods.sql', cfg.SQLPATH+'more.methods.sql', autocommit = False)
 
 res = db.named_recordset({'seq_from': 1000, 'seq_to': 1099, 'status': 5})
 print ('\r\nnamed_recordset:', json.dumps(res, indent = 2))
@@ -15,13 +15,17 @@ print ('\r\npositional_record:', json.dumps(res, indent = 2))
 res = db.named_value({'seq':54321})
 print ('\r\nnamed_value:', res)
 
-res = db.positional_none(200000, 99)
+res = db.positional_none(13, 123)
 print ('\r\npositional_none:', res)
+
+db.commit()
 
 res = db.Roman_64_numerals(10)
 print ('\r\nRoman_64_numerals:', json.dumps(res, indent = 2))
 
 res = db.cleanup({'seq':200000})
 print ('\r\ncleanup:', res)
+
+db.commit()
 
 conn.close()
