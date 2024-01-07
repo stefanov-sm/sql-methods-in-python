3ce576e175f203f7f60e58b2864e5df2e5b9baa6
Stefan Stefanov <stefanov.sm@abv.bg>
2024-01-07 10:35:49 +0200

    Update more.methods.sql

--- more.methods.sql
+++ more.methods.sql
@@ -4,9 +4,14 @@
 select n, to_char(n + 64, 'FMRN') rn
 from generate_series(1, ?, 1) as t(n);
 
---! {"name": "cleanup", "param_mode":"named", "returns": "none"}
-delete from tests.the_table
-where seq = :seq;
+--! {"name": "cleanup", "param_mode":"named", "returns": "value"}
+with t(n) as
+(
+ delete from tests.the_table
+ where seq >= :seq
+ returning 1
+)
+select count(n) from t;
 
 --! {"name": "commit", "param_mode":"none", "returns": "none"}
 commit;
