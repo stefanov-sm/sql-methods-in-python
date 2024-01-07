453913ba22f9bb8a0ae8e819273f0fbc0da05bb0
Stefan Stefanov <stefanov.sm@abv.bg>
2024-01-07 13:58:38 +0200

    Prime numbers in random status generation

--- demo.table.ddl.sql
+++ demo.table.ddl.sql
@@ -1,7 +1,7 @@
 create table tests.the_table
 (
   seq integer primary key not null,
-  ts timestamp default '2023-12-01'::date + '00:00:01'::interval * (random()*3600*24) not null,
+  ts timestamp default '2023-12-01'::date + interval '1 second' * (random()*3600*24) not null,
   value text default replace(gen_random_uuid()::text, '-', '') not null,
   status integer default (random() * 97)::integer % 11 not null
 );
