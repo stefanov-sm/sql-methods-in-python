a0301c8a6308a92836fb8b881a9cc03f44d5897d
Stefan Stefanov <stefanov.sm@abv.bg>
2024-01-08 11:22:31 +0200

    Update more.methods.sql

--- more.methods.sql
+++ more.methods.sql
@@ -1,10 +1,10 @@
 -- More demo queries
 
---! {"name": "Roman_64_numerals", "param_mode":"positional", "returns": "recordset"}
+--! {"name": "Roman_64_numerals", "param_mode": "positional", "returns": "recordset"}
 select n, to_char(n + 64, 'FMRN') rn
 from generate_series(1, ?, 1) as t(n);
 
---! {"name": "cleanup", "param_mode":"named", "returns": "value"}
+--! {"name": "cleanup", "param_mode": "named", "returns": "value"}
 with t(n) as
 (
  delete from tests.the_table
@@ -13,8 +13,8 @@ with t(n) as
 )
 select count(n) from t;
 
---! {"name": "commit", "param_mode":"none", "returns": "none"}
+--! {"name": "commit", "param_mode": "none", "returns": "none"}
 commit;
 
---! {"name": "rollback", "param_mode":"none", "returns": "none"}
+--! {"name": "rollback", "param_mode": "none", "returns": "none"}
 rollback;
