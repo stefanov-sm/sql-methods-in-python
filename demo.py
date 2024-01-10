0c4e81e355ab2d77c2577b51b847980b64c8395d
Stefan Stefanov <stefanov.sm@abv.bg>
2024-01-10 10:46:31 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,28 +1,30 @@
 import json
 import psycopg
-import demo_settings as cfg
-import dbgw
+from demo_settings import CONNECTIONSTRING, SQLPATH
+from dbgw import db_gateway
 
-with psycopg.connect(cfg.CONNECTIONSTRING) as conn:
+NEWLINE = '\r\n'
+with psycopg.connect(CONNECTIONSTRING) as conn:
 
-  db = dbgw.db_gateway(conn, cfg.SQLPATH + 'methods.sql', cfg.SQLPATH + 'more.methods.sql', autocommit = False)
+  db = db_gateway(conn, SQLPATH + 'methods.sql', SQLPATH + 'more.methods.sql', autocommit = False)
 
   res = db.named_recordset({'seq_from': 1000, 'seq_to': 1099, 'status': 5})
-  print ('\r\nnamed_recordset:', json.dumps(res, indent = 2))
+  print (NEWLINE + 'named_recordset:', json.dumps(res, indent = 2))
 
   res = db.positional_record(12345)
-  print ('\r\npositional_record:', json.dumps(res, indent = 2))
+  print (NEWLINE + 'positional_record:', json.dumps(res, indent = 2))
 
   res = db.named_value({'seq':54321})
-  print ('\r\nnamed_value:', res)
+  print (NEWLINE + 'named_value:', res)
 
   res = db.positional_none(13, 123)
   db.commit()
-  print ('\r\npositional_none:', res)
+  print (NEWLINE + 'positional_none:', res)
 
   res = db.Roman_64_numerals(10)
-  print ('\r\nRoman_64_numerals:', json.dumps(res, indent = 2))
+  print (NEWLINE + 'Plus 64 Roman:')
+  for rec in res: print(f">> {rec['n']}, {rec['rn']}")
 
-  res = db.cleanup({'seq':200000});
+  res = db.cleanup({'seq':200000})
   db.commit()
-  print ('\r\ncleanup:', res)
+  print (NEWLINE + 'cleanup:', res)
